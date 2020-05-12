8fb00f21c1a98c0ca39b7a33b8b017bc9661ec15
Mikhail Klimovich <mishaklimovich@tut.by>
2020-05-12 20:49:09 +0200

    Add car controller

--- rental-car-web/src/main/java/com/epam/brest/courses/web/CarController.java
+++ rental-car-web/src/main/java/com/epam/brest/courses/web/CarController.java
@@ -1,17 +1,18 @@
 package com.epam.brest.courses.web;
 
+import com.epam.brest.courses.model.Car;
 import com.epam.brest.courses.service.CarDtoService;
 import com.epam.brest.courses.service.CarService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
-/**
- * Car controller.
- */
+import java.util.Optional;
 
 @Controller
 public class CarController {
@@ -19,26 +20,65 @@ public class CarController {
     private static final Logger LOGGER = LoggerFactory.getLogger(CarController.class);
 
     private final CarDtoService carDtoService;
+    private final CarService carService;
 
-    private final CarService carSrvice;
-
-
-    @Autowired
-    public CarController(CarDtoService carDtoService, CarService carSrvice) {
+    public CarController(CarDtoService carDtoService, CarService carService) {
         this.carDtoService = carDtoService;
-        this.carSrvice = carSrvice;
+        this.carService = carService;
     }
 
-/**
- * Goto cars list page.
- *
- * @return view name
-*/
+    @GetMapping(value = "/cars")
+    public final String cars(Model model) {
 
-    public final String cars(Model model){
         LOGGER.debug("cars()");
-        model.addAttribute("cars",carDtoService.findAllCountRent());
+        model.addAttribute("cars", carDtoService.findAllCountRent());
         return "cars";
     }
 
+    /**
+     * Goto edit car page.
+     *
+     * @return view name
+     */
+    @GetMapping(value = "/car/{id}")
+    public final String gotoEditDepartmentPage(@PathVariable Integer id, Model model) {
+
+        LOGGER.debug("gotoEditCarPage({},{})", id, model);
+        Optional<Car> optionalCar = carService.findById(id);
+        if (optionalCar.isPresent()) {
+            model.addAttribute("isNew", false);
+            model.addAttribute("car", optionalCar.get());
+            return "car";
+        } else {
+            // TODO car not found - pass error message as parameter or handle not found error
+            return "redirect:cars";
+        }
+    }
+
+    /**
+     * Goto add car page.
+     *
+     * @return view name
+     */
+    @GetMapping(value = "/car")
+    public final String gotoAddCarPage(Model model) {
+
+        LOGGER.debug("gotoAddCarPage({})", model);
+        model.addAttribute("isNew", true);
+        model.addAttribute("car", new Car());
+        return "car";
+    }
+
+    /**
+     * Delete car.
+     *
+     * @return view name
+     */
+    @GetMapping(value = "/car/{id}/delete")
+    public final String deleteCarById(@PathVariable Integer id, Model model) {
+
+        LOGGER.debug("delete({},{})", id, model);
+        carService.delete(id);
+        return "redirect:/cars";
+    }
 }
--- rental-car-web/src/main/java/com/epam/brest/courses/web/HelloController.java
+++ rental-car-web/src/main/java/com/epam/brest/courses/web/HelloController.java
@@ -1,7 +1,9 @@
 package com.epam.brest.courses.web;
 
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,12 +14,13 @@ public class HelloController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(HelloController.class);
 
-    @GetMapping(value = "/hello")
-    public String hello(@RequestParam(value = "name", required = false, defaultValue = "World") String name,
-                        Model model) {
+    @GetMapping(value = "/home")
+    public String home(@RequestParam(value = "name", required = false, defaultValue = "World") String name,
+                       Model model) {
 
-        LOGGER.debug("hello(name:{})", name);
+        LOGGER.debug("home(name:{})", name);
         model.addAttribute("name", name);
         return "hello";
     }
+
 }
